f9a0dde9235ce25bf0c6bb6c7857e2ab3566b3f4
Aliaksandr Baravulia <alex.baraulia@gmail.com>
2020-04-12 13:31:39 +0300

    WebDriver implementation

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,11 +2,12 @@ package base;
 
 import driver.WebDriverProvider;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 import utill.ResourceUtils;
 
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import static utill.ResourceUtils.Resource.BROWSER;
 
@@ -19,12 +20,14 @@ public class BaseTest {
         this.webDriver = WebDriverProvider.get(browserProperties.get("browser").toString());
     }
 
-    @BeforeMethod
+    @BeforeTest
     public void setUp() {
+        WebDriverProvider.get(browserProperties.get("browser").toString());
         webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    @AfterMethod
+    @AfterTest
     public void tearDown() {
         WebDriverProvider.closeDriver();
     }
